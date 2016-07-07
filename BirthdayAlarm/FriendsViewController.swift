ffe8d54132315f9da4b5924f1a8e03bee2582a65
Tony Saavedra <ajsaavedrax@gmail.com>
2016-07-06 23:02:33 -0700

    Initial birthday alert repo with blank table view

---
+++ BirthdayAlarm/FriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.swift
+//  BirthdayAlarm
+//
+//  Created by Tony Saavedra on 7/6/16.
+//  Copyright © 2016 Tony Saavedra. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UITableViewController {
+    
+}
